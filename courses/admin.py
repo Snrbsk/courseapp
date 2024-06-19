e6b366e0473ecfd6bd09480d699be48ec4af45be
Soner Basak <Snrbsk.official@gmail.com>
2024-06-19 17:56:48 +0300

    Django-AdminPanel

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from courses.models import Course,Categories
 
 # Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("title","isActive","slug")
+    list_display_links = ("title","slug")
+    readonly_fields = ("slug",)
+    list_filter = ("isActive",)
+    list_editable = ("isActive",)
+    search_fields = ("title","description")
+
+@admin.register(Categories)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
--- courses/models.py
+++ courses/models.py
@@ -8,7 +8,7 @@ class Course(models.Model):
     imageUrl = models.CharField(max_length=50)
     date = models.DateField()
     isActive = models.BooleanField()
-    slug = models.SlugField(default= "", null= False, unique= True, db_index = True)
+    slug = models.SlugField(default= "", blank= True, null= False, unique= True, db_index = True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
@@ -20,4 +20,7 @@ class Course(models.Model):
 class Categories(models.Model):
     name = models.CharField(max_length=50)
     slug = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
     
