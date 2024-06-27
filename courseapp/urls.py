1f2cd7b76918e46f00e84c891a484d1813c42c0f
Soner Basak <Snrbsk.official@gmail.com>
2024-06-27 21:35:16 +0300

    Finish

--- courseapp/urls.py
+++ courseapp/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('course/', include('courses.urls')),    
     path('account/', include('account.urls')),
     path('', include('pages.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) \
++ static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
  
